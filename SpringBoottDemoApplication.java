1fe79883cce132b4ec37932ffe159bba6f7c121d
NigelBynum <136828778+NigelBynum@users.noreply.github.com>
2023-08-16 16:27:12 -0400

    Add files via upload
    complete

---
+++ SpringBoottDemoApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.SpringBoottDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoottDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoottDemoApplication.class, args);
+	}
+
+}
